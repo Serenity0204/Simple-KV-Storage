b66696cf8fe43047be2f4fc753c6157ba2558727
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-03-28 11:41:38 -0700

    added functions for simple kv, next should consider a serializer for template data

--- includes/simple_kv.h
+++ includes/simple_kv.h
@@ -8,19 +8,54 @@ class Simple_KV
 {
 private:
     BinaryFileIO _io;
-    HashTable<T> _table;
+    HashTable<int> _table;
+
 public:
     Simple_KV();
     ~Simple_KV();
+    void put(string key, T value);
+    T get(string key);
+    bool exist(string key);
+    void remove(string key);
+    void display();
 };
 
 template <class T>
-Simple_KV<T>::Simple_KV(/* args */)
+Simple_KV<T>::Simple_KV()
 {
+    this->_io = BinaryFileIO();
+    this->_table = HashTable<int>();
 }
 
 template <class T>
 Simple_KV<T>::~Simple_KV()
+{
+}
+
+template <class T>
+void Simple_KV<T>::put(string key, T value)
+{
+}
+
+template <class T>
+T Simple_KV<T>::get(string key)
+{
+}
+
+template <class T>
+bool Simple_KV<T>::exist(string key)
+{
+    if (!this->_table.count(key)) return false;
+    return true;
+}
+
+template <class T>
+void Simple_KV<T>::remove(string key)
+{
+}
+
+template <class T>
+void Simple_KV<T>::display()
 {
 }
 
